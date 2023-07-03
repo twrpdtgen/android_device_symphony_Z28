e5456003351727f277deb942802428478c3d3157
Alhadi1024 <117118314+Alhadi1024@users.noreply.github.com>
2023-07-04 03:26:35 +0600

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,7 @@ TW_INCLUDE_FASTBOOTD := true
 
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
+#TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 BOARD_INCLUDE_RECOVERY_DTBO := true
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
